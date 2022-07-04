1130c9ad17d9096997b7a706f96614717f45aabf
wellersonjesus <wellersonjesus@hotmail.com>
2022-07-04 07:59:26 -0300

    chore: add jest and testcontroller.spect

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
-  trasform: {
+  transform: {
     ".+\\.ts$": "ts-jest",
   },
 };
