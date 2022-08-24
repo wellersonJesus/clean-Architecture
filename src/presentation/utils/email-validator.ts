cc6b536fc4697c867701facbd92c9bfafd2b0bb3
wellersonjesus <wellersonjesus@hotmail.com>
2022-08-24 07:12:03 -0300

    chore: changed

--- src/presentation/utils/email-validator.ts
+++ src/presentation/utils/email-validator.ts
@@ -1,9 +1,7 @@
-import validator from "validator";
 import { EmailValidator } from "../protocols/email-validator";
 
 export class EmailValidatorAdapter implements EmailValidator {
 	isValid(email: string): boolean {
 		return false;
-		return validator.isEmail(email);
 	}
 }
