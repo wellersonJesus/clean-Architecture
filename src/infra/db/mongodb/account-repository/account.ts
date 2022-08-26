033505a3dfd92f12c12739f55b901858fe8f8100
wellersonjesus <wellersonjesus@hotmail.com>
2022-08-26 07:15:47 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,12 +1,14 @@
 import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
-import { AddAccountModel } from "../../../../data/protocols/usecases/db-add-account-protocols";
 import { AccountModel } from "../../../../domain/models/account";
+import { AddAccountModel } from "../../../../domain/usercases/add-account";
 import { MongoHelper } from "../helpers/mongo-helper";
 
 export class AccountMongoRepository implements AddAccountRepository {
 	async add(accountData: AddAccountModel): Promise<AccountModel> {
 		const accountCollection = MongoHelper.getCollection("accounts");
 		const result = await accountCollection.insertOne(accountData);
-		return MongoHelper.map(result.ops[0]);
+		const account = result.ops[0];
+		const { _id, ...accountWithoutId } = account;
+		return Object.assign({}, accountWithoutId, { id: _id });
 	}
 }
