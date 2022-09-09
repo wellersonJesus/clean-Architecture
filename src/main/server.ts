4902f8df66cf43bb45085969fb930fc1ffd044d9
wellersonjesus <wellersonjesus@hotmail.com>
2022-09-09 10:03:31 -0300

    feat: add ExpressRouteAdapter

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from "./config/app";
+import { MongoHelper } from "../infra/db/mongodb/helpers/mongo-helper";
+import env from "./config/env";
 
-app.listen(5030, () => console.log("Server running at http://localhost:5030"));
+MongoHelper.connect(env.mongoUrl)
+	.then(async () => {
+		const app = (await import("./config/app")).default;
+		app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
+	})
+	.catch(console.error);
